e29d304508fcf4e7a3b5be6ad49be824a215f14e
David McCoy <davidmccoy@Po.local>
2021-09-23 11:05:10 -0700

    change library path server

--- Analysis/update_data/03_ensemble_ML_variable_importance.R
+++ Analysis/update_data/03_ensemble_ML_variable_importance.R
@@ -1,11 +1,11 @@
-# switch up library path for the slurm monster
-r_libdir <- Sys.getenv("R_LIBDIR")
-
-# set user-specific package library
-if (grepl("savio2", Sys.info()["nodename"])) {
-  .libPaths(r_libdir)
-  Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
-}
+# # switch up library path for the slurm monster
+# r_libdir <- Sys.getenv("R_LIBDIR")
+# 
+# # set user-specific package library
+# if (grepl("savio2", Sys.info()["nodename"])) {
+#   .libPaths(r_libdir)
+#   Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+# }
 
 
 library(sl3)
